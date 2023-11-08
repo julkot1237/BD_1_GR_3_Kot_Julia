403f26fd0f324ba595a926510127d2e3069160c2
julkot1237 <147729899+julkot1237@users.noreply.github.com>
2023-11-08 15:17:33 +0100

    Add files via upload
    część pierwsza ćwiczenia 5

---
+++ pensje_insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO rozliczenia.pensje(
+	id_pensji, stanowisko, kwota, id_premii)
+VALUES(201, 'szef', 550, 303),
+(202, 'sprzedawca', 200, 307),
+(203, 'starszy sprzedawca', 400, 348),
+(204, 'kierownik departamentu', 660, 377),
+(205, 'kierownik  działu męskiego', 880, 390),
+(206, 'kierownik działu damskiego', 660, 398),
+(207, 'hr', 1000, 340),
+(208, 'pani sprzątająca', 450, 350),
+(209, 'kierownik salonu', 660, 388),
+(210, 'dekorator', 200, 399);
